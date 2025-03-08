255920f0b8c8a7fcce46b0f4740fb42d8b763a70
Emanuelpa <epatinov@eafit.edu.co>
2025-03-08 10:03:28 -0500

    feat: user class created and seeded

--- FutbolTrading/app/Models/User.php
+++ FutbolTrading/app/Models/User.php
@@ -3,46 +3,122 @@
 namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
+
+use DateTime;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 
 class User extends Authenticatable
 {
-    /** @use HasFactory<\Database\Factories\UserFactory> */
     use HasFactory, Notifiable;
 
     /**
-     * The attributes that are mass assignable.
-     *
-     * @var list<string>
+     * USER ATTRIBUTES
+     * $this->attributes['id'] - int - contains the product primary key (id)
+     * $this->attributes['name'] - string - contains the user name
+     * $this->attributes['email'] - string - contains the user email, unique
+     * $this->attributes['emailVerifiedAt'] - DateTime - date and time of the email verification
+     * $this->attributes['password'] - string - contain the user password
+     * $this->attributes['role'] - string - contain the user role (admin, user)
+     * $this->attributes['budget'] - float - contain the user budget
+     * $this->attributes['remember_token'] - string - the remember me field
+     * $this->attributes['created_at'] - DateTime - contains the date and time of the user creation
+     * $this->attributes['updated_at'] - DateTime - contains the date and time of the user last update
      */
     protected $fillable = [
         'name',
         'email',
         'password',
+        'role',
+        'budget',
     ];
 
-    /**
-     * The attributes that should be hidden for serialization.
-     *
-     * @var list<string>
-     */
     protected $hidden = [
         'password',
         'remember_token',
     ];
 
-    /**
-     * Get the attributes that should be cast.
-     *
-     * @return array<string, string>
-     */
     protected function casts(): array
     {
         return [
-            'email_verified_at' => 'datetime',
+            'emailVerifiedAt' => 'datetime',
             'password' => 'hashed',
         ];
+    }
+
+    public function getId(): int
+    {
+        return $this->attributes['id'];
+    }
+
+    public function setName(string $name): void
+    {
+        $this->attributes['name'] = $name;
+    }
+
+    public function getName(): string
+    {
+        return $this->attributes['name'];
+    }
+
+    public function setEmail(string $email): void
+    {
+        $this->attributes['email'] = $email;
+    }
+
+    public function getEmail(): string
+    {
+        return $this->attributes['email'];
+    }
+
+    public function getEmailVerifiedAt(): DateTime
+    {
+        return $this->attributes['emailVerifiedAt'];
+    }
+
+    public function setPassword(string $password): void
+    {
+        $this->attributes['password'] = $password;
+    }
+
+    public function getPassword(): string
+    {
+        return $this->attributes['password'];
+    }
+
+    public function setRole(string $role): void
+    {
+        $this->attributes['role'] = $role;
+    }
+
+    public function getRole(): string
+    {
+        return $this->attributes['role'];
+    }
+
+    public function setBudget(float $budget): void
+    {
+        $this->attributes['budget'] = $budget;
+    }
+
+    public function getBudget(): float
+    {
+        return $this->attributes['budget'];
+    }
+
+    public function getRememberToken(): string
+    {
+        return $this->attributes['remember_token'];
+    }
+
+    public function getCreatedAt(): DateTime
+    {
+        return $this->attributes['created_at'];
+    }
+
+    public function getUpdatedAt(): DateTime
+    {
+        return $this->attributes['updated_at'];
     }
 }
