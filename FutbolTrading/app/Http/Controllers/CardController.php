9b99c7173db484262e149324404d99194b43d48a
mlondono13 <mlondonol@eafit.edu.co>
2025-03-13 15:11:19 -0500

    feat: Card controller created

---
+++ FutbolTrading/app/Http/Controllers/CardController.php
@@ -0,0 +1,70 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Card;
+use Illuminate\Http\RedirectResponse;
+use Illuminate\Http\Request;
+use Illuminate\View\View;
+
+class CardController extends Controller
+{
+    public function index(): View
+    {
+        $viewData = [];
+        $viewData['title'] = 'Cards - Online Store';
+        $viewData['subtitle'] = 'List of cards';
+        $viewData['cards'] = Card::all();
+
+        return view('card.index')->with('viewData', $viewData);
+    }
+
+    public function show(string $id): View
+    {
+        $viewData = [];
+        $card = Card::findOrFail($id);
+        $viewData['title'] = $card->getName().' - Online Store';
+        $viewData['subtitle'] = $card->getName().' - Product information';
+        $viewData['card'] = [
+            'id' => $card->getId(),
+            'name' => $card->getName(),
+            'description' => $card->getDescription(),
+            'image' => $card->getImage(),
+            'price' => $card->getPrice(),
+            'quantity' => $card->getQuantity(),
+            'created_at' => $card->created_at,
+            'updated_at' => $card->updated_at,
+        ];
+
+        return view('card.show')->with('viewData', $viewData);
+    }
+
+    public function create(): View
+    {
+        $viewData = [];
+        $viewData['title'] = 'Create card';
+
+        return view('card.create')->with('viewData', $viewData);
+    }
+
+    public function save(Request $request): RedirectResponse
+    {
+        $card = new Card;
+        $card->setName($request->input('name'));
+        $card->setDescription($request->input('description'));
+        $card->setImage($request->input('image'));
+        $card->setPrice($request->input('price'));
+        $card->setQuantity($request->input('quantity'));
+        $card->save();
+
+        return redirect()->back()->with('success', 'Item created successfully');
+    }
+
+    public function destroy($id): RedirectResponse
+    {
+        $card = Card::findOrFail($id);
+        $card->delete();
+
+        return redirect()->route('card.index')->with('success', 'Card deleted successfully.');
+    }
+}
