199cedb28b3568c10a6ed524a983e4114a9921bd
Tomas <tomaspinedanaranjo@gmail.com>
2025-03-14 10:05:45 -0500

    fix: home page

---
+++ FutbolTrading/resources/lang/en/Home.php
@@ -0,0 +1,11 @@
+<?php
+
+return [
+    'welcome' => '¡Welcome!',
+    'transform' => 'Transform Your Collection with Our Soccer Cards',
+    'transformDescription' => 'We help collectors find rare, limited-edition, and exclusive soccer cards to complete your collections and showcase your passion.',
+    'authenticCards' => 'Authentic Cards',
+    'authenticCardsDescription' => 'Our cards are genuine collectibles and authenticated by experts.',
+    'rareFinds' => 'Rare Finds',
+    'rareFindsDescription' => 'Get exclusive cards from limited editions.',
+];
