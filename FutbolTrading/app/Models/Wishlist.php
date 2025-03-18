e0b8905582998ab44980de138be18c35c2cfe419
mlondono13 <mlondonol@eafit.edu.co>
2025-03-17 22:24:06 -0500

    feat: wishlist class implemented

---
+++ FutbolTrading/app/Models/Wishlist.php
@@ -0,0 +1,74 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+
+class Wishlist extends Model
+{
+    /**
+     * CARD ATTRIBUTES
+     * $this->attributes['id'] - int - contains the wishlist primary key (id)
+     * $this->attributes['created_at'] - DateTime - contains the date and time of the Wishlist creation
+     * $this->attributes['updated_at'] - DateTime - contains the date and time of the Wishlist last update
+     * $this->user - User[] - contains the associated User
+     * $this->cards - Card[] - contains the associated Cards
+     */
+
+    protected $fillable = ['user'];
+
+    public static function validate($request): void
+    {
+        $request->validate([
+            'user' => 'required|int',
+            'card' => 'nullable|int'
+        ]);
+    }
+
+    public function getId(): int
+    {
+        return $this->attributes['id'];
+    }
+
+    public function getCreatedAt(): string
+    {
+        return $this->attributes['created_at'];
+    }
+
+    public function getUpdatedAt(): string
+    {
+        return $this->attributes['updated_at'];
+    }
+
+    public function user() : BelongsTo 
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function getUser(): User
+    {
+        return $this->attributes['user'];
+    }
+
+    public function setUser(int $user): void
+    {
+        $this->attributes['user'] = $user;
+    }
+
+    public function cards(): HasMany
+    {
+        return $this->hasMany(Card::class);
+    }
+
+    public function getCards(): Card
+    {
+        return $this->attributes['cards'];
+    }
+
+    public function setCards(int $cards):void
+    {
+        $this->attributes['cards'] = $cards;
+    }
+}
---
+++ FutbolTrading/database/migrations/2025_03_18_004813_create_wishlist_table.php.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('wishlists', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('user')->unique();
+            $table->unsignedBigInteger('card');
+            $table->timestamps();
+            $table->foreign('user')->references('id')->on('users');
+            $table->foreign('card')->references('id')->on('cards')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('wishlists');
+    }
+};
