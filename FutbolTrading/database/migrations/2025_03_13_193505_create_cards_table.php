bd257e125191bc2a0c8816958f0a6c068602de9b
mlondono13 <mlondonol@eafit.edu.co>
2025-03-19 18:21:56 -0500

    fix: item and wishlist columns added

--- FutbolTrading/database/migrations/2025_03_13_193505_create_cards_table.php
+++ FutbolTrading/database/migrations/2025_03_13_193505_create_cards_table.php
@@ -18,6 +18,8 @@ return new class extends Migration
             $table->text('image');
             $table->float('price');
             $table->integer('quantity');
+            $table->unsignedBigInteger('item');
+            $table->unsignedBigInteger('wishlist');
             $table->timestamps();
             $table->foreign('item')->references('id')->on('items');
             $table->foreign('wishlist')->references('id')->on('wishlists');
